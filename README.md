cda50787faedc6d0dec019efb0165b8133221d0d
bernardo.almeida <bernardo.almeida94@gmail.com>
2022-11-15 17:02:07 +0100

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Neural_machine_translation
+
+The goal was to create and train a [sequence-to-sequence](https://developers.google.com/machine-learning/glossary#sequence-to-sequence-task) [Transformer](https://developers.google.com/machine-learning/glossary#Transformer) model to translate [Portuguese into English](https://www.tensorflow.org/datasets/catalog/ted_hrlr_translate#ted_hrlr_translatept_to_en). The Transformer was originally proposed in ["Attention is all you need"](https://arxiv.org/abs/1706.03762) by Vaswani et al. (2017).
+Transformers are deep neural networks that replace CNNs and RNNs with [self-attention](https://developers.google.com/machine-learning/glossary#self-attention). Self attention allows Transformers to easily transmit information across the input sequences.
+
+I followed this tutorial: https://www.tensorflow.org/text/tutorials/transformer  
+
+Steps:  
+- Prepare the data.
+- Implement necessary components:
+  - Positional embeddings.
+  - Attention layers.
+  - The encoder and decoder.
+- Build & train the Transformer.
+- Generate translations.
+- Export the model.
+
+I used the TensorFlow Datasets to load the Portuguese-English translation dataset Talks Open Translation Project. This dataset contains approximately 52,000 training, 1,200 validation and 1,800 test examples.
+ 
+## Conda environment
+
+```
+conda create -n Practice_tf_gpu tensorflow-gpu matplotlib ipykernel pandas seaborn keras-gpu python=3.7
+conda activate Practice_tf_gpu
+pip install kaggle
+pip install transformers
+pip install tensorflow_datasets
+pip install tensorflow-text
+```
+
+## Code
+Jupyter notebook with the different steps: Translation_transformer.ipynb  
+Python script to run model from terminal: Translation_transformer.py  
+
+## Translate Portuguese into English using final model
+
+```
+python ./Translate_port_eng.py "este é o primeiro livro que eu fiz."
+```
---
+++ Translation_transformer.py
@@ -0,0 +1,285 @@
+
+## run as: 
+# cd /groups/stark/almeida/DeepLearning_practice/Neural_machine_translation/
+# my_bsub_gridengine -P g -G "gpu:1" -m 15 -T '2-00:00:00' -o log_training -n Training "python ./Translation_transformer.py"
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # INFO and WARNING messages are not printed
+
+import logging
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import tensorflow_text
+tf.config.experimental.list_physical_devices('GPU')
+
+### Download the dataset
+print("\nDownload the dataset ...\n")
+
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                               with_info=True,
+                               as_supervised=True)
+
+train_examples, val_examples = examples['train'], examples['validation']
+
+### Set up the tokenizer
+print("\nSet up the tokenizer ...\n")
+
+model_name = 'ted_hrlr_translate_pt_en_converter'
+tokenizers = tf.saved_model.load(model_name)
+
+### Set up a data pipeline with tf.data
+print("\nSet up a data pipeline with tf.data ...\n")
+
+MAX_TOKENS=128
+def prepare_batch(pt, en):
+    pt = tokenizers.pt.tokenize(pt)      # Output is ragged.
+    pt = pt[:, :MAX_TOKENS]    # Trim to MAX_TOKENS.
+    pt = pt.to_tensor()  # Convert to 0-padded dense Tensor
+
+    en = tokenizers.en.tokenize(en)
+    en = en[:, :(MAX_TOKENS+1)]
+    en_inputs = en[:, :-1].to_tensor()  # Drop the [END] tokens
+    en_labels = en[:, 1:].to_tensor()   # Drop the [START] tokens
+
+    return (pt, en_inputs), en_labels
+
+BUFFER_SIZE = 20000
+BATCH_SIZE = 64
+
+def make_batches(ds):
+  return (
+      ds
+      .shuffle(BUFFER_SIZE)
+      .batch(BATCH_SIZE)
+      .map(prepare_batch, tf.data.AUTOTUNE)
+      .prefetch(buffer_size=tf.data.AUTOTUNE))
+
+# Create training and validation set batches.
+train_batches = make_batches(train_examples)
+val_batches = make_batches(val_examples)
+
+### Build transformer
+print("\nLoad transformer architecture ...\n")
+
+from Transformer_architecture import *
+
+# Hyperparameters
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+dropout_rate = 0.1
+
+# Try it out
+transformer = Transformer(
+    num_layers=num_layers,
+    d_model=d_model,
+    num_heads=num_heads,
+    dff=dff,
+    input_vocab_size=tokenizers.pt.get_vocab_size().numpy(),
+    target_vocab_size=tokenizers.en.get_vocab_size().numpy(),
+    dropout_rate=dropout_rate)
+
+for (pt, en), en_labels in train_batches.take(1):
+  break
+output = transformer((pt, en))
+
+transformer.summary()
+
+### Training
+print("\nTraining ...\n")
+
+# optimizer
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, d_model, warmup_steps=4000):
+    super().__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    step = tf.cast(step, dtype=tf.float32)
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                     epsilon=1e-9)
+
+# loss and metrics
+def masked_loss(label, pred):
+  mask = label != 0
+  loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+  loss = loss_object(label, pred)
+
+  mask = tf.cast(mask, dtype=loss.dtype)
+  loss *= mask
+
+  loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
+  return loss
+
+
+def masked_accuracy(label, pred):
+  pred = tf.argmax(pred, axis=2)
+  label = tf.cast(label, pred.dtype)
+  match = label == pred
+
+  mask = label != 0
+
+  match = match & mask
+
+  match = tf.cast(match, dtype=tf.float32)
+  mask = tf.cast(mask, dtype=tf.float32)
+  return tf.reduce_sum(match)/tf.reduce_sum(mask)
+
+# callbacks
+checkpoint_filepath = 'my_best_model.hdf5'
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='val_masked_accuracy',
+    mode='max',
+    save_best_only=True)
+
+# Train the model
+transformer.compile(
+    loss=masked_loss,
+    optimizer=optimizer,
+    metrics=[masked_accuracy])
+
+history = transformer.fit(train_batches,
+                          epochs=40,
+                          validation_data=val_batches,
+                          callbacks=[model_checkpoint_callback])
+
+# Save model
+model.save('Model_translator_final.h5')
+# Save history
+import numpy as np
+np.save('Model_translator_final_history.npy',history.history)
+
+### Model evaluation
+print("\nModel evaluation ...\n")
+import matplotlib.pyplot as plt
+# plot history
+plt.ion()
+fig = plt.figure(figsize=(14,4))
+subfig = fig.add_subplot(122)
+subfig.plot(history.history['accuracy'], label="training")
+if history.history['val_accuracy'] is not None:
+    subfig.plot(history.history['val_accuracy'], label="validation")
+subfig.set_title('Model Accuracy')
+subfig.set_xlabel('Epoch')
+subfig.legend(loc='upper left')
+subfig = fig.add_subplot(121)
+subfig.plot(history.history['loss'], label="training")
+if history.history['val_loss'] is not None:
+    subfig.plot(history.history['val_loss'], label="validation")
+subfig.set_title('Model Loss')
+subfig.set_xlabel('Epoch')
+subfig.legend(loc='upper left')
+plt.ioff()
+plt.savefig('Model_translator_performance.pdf')  
+
+### Run inference
+print("\nRun inference ...\n")
+
+class Translator(tf.Module):
+  def __init__(self, tokenizers, transformer):
+    self.tokenizers = tokenizers
+    self.transformer = transformer
+
+  def __call__(self, sentence, max_length=MAX_TOKENS):
+    # The input sentence is Portuguese, hence adding the `[START]` and `[END]` tokens.
+    assert isinstance(sentence, tf.Tensor)
+    if len(sentence.shape) == 0:
+      sentence = sentence[tf.newaxis]
+
+    sentence = self.tokenizers.pt.tokenize(sentence).to_tensor()
+
+    encoder_input = sentence
+
+    # As the output language is English, initialize the output with the
+    # English `[START]` token.
+    start_end = self.tokenizers.en.tokenize([''])[0]
+    start = start_end[0][tf.newaxis]
+    end = start_end[1][tf.newaxis]
+
+    # `tf.TensorArray` is required here (instead of a Python list), so that the
+    # dynamic-loop can be traced by `tf.function`.
+    output_array = tf.TensorArray(dtype=tf.int64, size=0, dynamic_size=True)
+    output_array = output_array.write(0, start)
+
+    for i in tf.range(max_length):
+      output = tf.transpose(output_array.stack())
+      predictions = self.transformer([encoder_input, output], training=False)
+
+      # Select the last token from the `seq_len` dimension.
+      predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
+
+      predicted_id = tf.argmax(predictions, axis=-1)
+
+      # Concatenate the `predicted_id` to the output which is given to the
+      # decoder as its input.
+      output_array = output_array.write(i+1, predicted_id[0])
+
+      if predicted_id == end:
+        break
+
+    output = tf.transpose(output_array.stack())
+    # The output shape is `(1, tokens)`.
+    text = tokenizers.en.detokenize(output)[0]  # Shape: `()`.
+
+    tokens = tokenizers.en.lookup(output)[0]
+
+    # `tf.function` prevents us from using the attention_weights that were
+    # calculated on the last iteration of the loop.
+    # So, recalculate them outside the loop.
+    self.transformer([encoder_input, output[:,:-1]], training=False)
+    attention_weights = self.transformer.decoder.last_attn_scores
+
+    return text, tokens, attention_weights
+
+translator = Translator(tokenizers, transformer)
+
+def print_translation(sentence, tokens, ground_truth):
+  print(f'{"Input:":15s}: {sentence}')
+  print(f'{"Prediction":15s}: {tokens.numpy().decode("utf-8")}')
+  print(f'{"Ground truth":15s}: {ground_truth}')
+
+sentence = 'este é um problema que temos que resolver.'
+ground_truth = 'this is a problem we have to solve .'
+
+translated_text, translated_tokens, attention_weights = translator(
+    tf.constant(sentence))
+print_translation(sentence, translated_text, ground_truth)
+
+### Export model
+print("\nExport model ...\n")
+
+class ExportTranslator(tf.Module):
+  def __init__(self, translator):
+    self.translator = translator
+
+  @tf.function(input_signature=[tf.TensorSpec(shape=[], dtype=tf.string)])
+  def __call__(self, sentence):
+    (result,
+     tokens,
+     attention_weights) = self.translator(sentence, max_length=MAX_TOKENS)
+
+    return result
+
+translator = ExportTranslator(translator)
+translator('este é o primeiro livro que eu fiz.').numpy()
+
+tf.saved_model.save(translator, export_dir='translator_final')
